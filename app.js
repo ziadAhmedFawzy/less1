e22a7a806d942eb6615efb19c7544d78216e1929
eng/ziad <ziadbusiness2000@gmail.com>
2024-10-25 23:26:27 +0300

    connect mongo

--- app.js
+++ app.js
@@ -9,12 +9,22 @@ const port = 3000;
 // in path / display in body hello world
 app.get('/', (req,res) => {
     // res.send('hello, ziad'); // to write text in html specifically in body
-    // res.sendFile('./view/homepage.html', {root: __dirname}) to display in html file
+    res.sendFile('./view/homepage.html', {root: __dirname}) //to display in html file
 })
 
-// define listen and funcrion 
-app.listen(port, () => {
-    console.log('app listen from port 3000 :)');
+const mongoose = require('mongoose');
+
+// connect to mongodb
+// pass : qh63O5tLKPtkmqNA
+mongoose.
+connect("mongodb+srv://ziadFawzy:qh63O5tLKPtkmqNA@cluster0.t8gc3.mongodb.net/get-data?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    // define listen and funcrion 
+    app.listen(port, () => {
+        console.log('app listen from port 3000 :)');
+    })
+}).catch((err) => {
+    console.log(err)
 })
 
 /*
